ed72291f8254e5af6da0f2b99b32a04630aa4fab
Aaron Farber <aaronfarber9@gmail.com>
2018-12-27 20:15:07 -0800

    finished navbar minus home icon

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StaticQuery, graphql } from "gatsby"
 import '../all.sass'
 import mainSectionStyles from "./main-section.module.css"
+import SocialMediaSidebar from "../SocialMediaSidebar/SocialMediaSidebar";
 import email from "../../img/envelope-solid.svg"
 
 const MainSection = ({ children }) => (
@@ -25,44 +26,7 @@ const MainSection = ({ children }) => (
             <h2 className={mainSectionStyles.subtitle}>{data.site.siteMetadata.description}
             </h2>
           </div>
-          <div className={mainSectionStyles.socialcontainer}>
-            <aside className={mainSectionStyles.social}>
-            <a href="https://www.twitter.com/life_is_a_sport"
-                title="Twitter - life is a sport" 
-                rel="noopener noreferrer"
-                aria-label="Twitter profile for Aaron Farber life_is_a_sport"
-                target="_blank"
-              >
-                <div className={`${mainSectionStyles.socialicon} ${mainSectionStyles.tw}`}></div>
-            </a>
-              <span aria-hidden="true" className={mainSectionStyles.tick}></span>
-            <a href="https://www.youtube.com"
-                title="YouTube" 
-                rel="noopener noreferrer"
-                aria-label="Aaron Farber YouTube channel external link"
-                target="_blank"
-              >
-                <div className={`${mainSectionStyles.socialicon} ${mainSectionStyles.yt}`}></div>
-              </a>
-              <span aria-hidden="true" className={mainSectionStyles.tick}></span>
-              <a href="https://www.github.com/aaronbnb"
-                title="GitHub aaronbnb" 
-                rel="noopener noreferrer"
-                aria-label="Aaron Farber's GitHub profile aaronbnb external link"
-                target="_blank"
-              >
-                <div className={`${mainSectionStyles.socialicon} ${mainSectionStyles.gh}`}></div>
-              </a>
-              <span aria-hidden="true" className={mainSectionStyles.tick}></span>
-              <a href="mailto:aaronfarber9@gmail.com"
-                title="Email Aaron" 
-                rel="noopener noreferrer"
-                aria-label="Send email to aaronfarber9@gmail.com"
-              >
-             <div className={`${mainSectionStyles.socialicon} ${mainSectionStyles.em}`}></div>
-              </a>
-            </aside>
-          </div>
+          <SocialMediaSidebar />
           </div>
           <div>
         </div>
@@ -73,4 +37,3 @@ const MainSection = ({ children }) => (
 )
 
 export default MainSection
-// style={{'display': 'none'}}
---
+++ src/components/SocialMediaSidebar/SocialMediaSidebar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import '../all.sass'
+import sidebarStyles from "./social-media-sidebar.module.css"
+
+const SocialMediaSidebar = () => (
+          <div className={sidebarStyles.socialcontainer}>
+            <aside className={sidebarStyles.social}>
+            <a href="https://www.twitter.com/life_is_a_sport"
+                title="Twitter - life is a sport" 
+                rel="noopener noreferrer"
+                aria-label="Twitter profile for Aaron Farber life_is_a_sport"
+                target="_blank"
+              >
+                <div className={`${sidebarStyles.socialicon} ${sidebarStyles.tw}`}></div>
+            </a>
+              <span aria-hidden="true" className={sidebarStyles.tick}></span>
+            <a href="https://www.youtube.com"
+                title="YouTube" 
+                rel="noopener noreferrer"
+                aria-label="Aaron Farber YouTube channel external link"
+                target="_blank"
+              >
+                <div className={`${sidebarStyles.socialicon} ${sidebarStyles.yt}`}></div>
+              </a>
+              <span aria-hidden="true" className={sidebarStyles.tick}></span>
+              <a href="https://www.github.com/aaronbnb"
+                title="GitHub aaronbnb" 
+                rel="noopener noreferrer"
+                aria-label="Aaron Farber's GitHub profile aaronbnb external link"
+                target="_blank"
+              >
+                <div className={`${sidebarStyles.socialicon} ${sidebarStyles.gh}`}></div>
+              </a>
+              <span aria-hidden="true" className={sidebarStyles.tick}></span>
+              <a href="mailto:aaronfarber9@gmail.com"
+                title="Email Aaron" 
+                rel="noopener noreferrer"
+                aria-label="Send email to aaronfarber9@gmail.com"
+              >
+             <div className={`${sidebarStyles.socialicon} ${sidebarStyles.em}`}></div>
+              </a>
+            </aside>
+          </div>
+    );
+
+export default SocialMediaSidebar;
