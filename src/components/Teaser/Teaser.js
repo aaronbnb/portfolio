f0824752d50c7e72c4a10d8c9c9db98460596e06
Aaron Farber <aaronfarber9@gmail.com>
2019-05-28 18:20:58 -0700

    changed ux copy and got rid of some navbar links not ready for showtime

--- src/components/Teaser/Teaser.js
+++ src/components/Teaser/Teaser.js
@@ -28,7 +28,7 @@ const Teaser = ({ children }) => (
                         <strong>I help organizations create inclusive online experiences and achieve accessibility compliance goals.</strong> 
                         </p>
                         <p className={teaserStyle.header}>
-                        I'm an IAAP-certified Web Accessibility Specialist and Technology Director at <a href="https://beaccessible.com" rel="noopener noreferrer" className={teaserStyle.link}>Be Accessible</a>. I identify accessibility challenges and potential legal risks in the design of our clients' websites, and provide recommended fixes. I've handled development for WordPress, Shopify, and even custom React applications. 
+                        I'm an IAAP-certified Web Accessibility Specialist and Technology Director at <a href="https://beaccessible.com" rel="noopener noreferrer" className={teaserStyle.link}>Be Accessible</a>. For our clients, I identify accessibility challenges and potential legal risks in the design of their website, and provide recommended fixes. I've handled development for WordPress, Shopify, and even custom React applications. 
                         </p>
                     </div>
                     <br></br> 
@@ -37,7 +37,7 @@ const Teaser = ({ children }) => (
                         <strong>I teach web development and cyber security, helping tens of students learn to build an accessible, secure internet, and shift careers into technology - just like I did.</strong> 
                         </p>
                         <p className={teaserStyle.header}>
-                        I serve as a Teaching Assistant for the Cyber Security program at <a href="https://bootcamp.uclaextension.edu/" rel="noopener noreferrer" className={teaserStyle.link}>UCLA Extension</a>. I run free workshops, training technology professionals how to build applications that support the broadest circle of people, particularly those that rely on assistive technology.
+                        I serve as a Teaching Assistant for the Cyber Security program at <a href="https://bootcamp.uclaextension.edu/" rel="noopener noreferrer" className={teaserStyle.link}>UCLA Extension</a>. I run free workshops, training developers how to build applications that support the broadest circle of people, particularly those that rely on assistive technology.
                         </p>
                     </div>
                     
